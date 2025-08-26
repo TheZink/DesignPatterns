092a1f61e0a3195faac61ad5c5ea0fe492602836
TheZink <ilkka.sinkonen@gmail.com>
2025-08-26 14:04:27 +0300

    Removed folder

--- abstract_factory/src/main/UIFactory.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public abstract class UIFactory {
-
-    public abstract Button createButton(String text);
-    public abstract Checkbox createCheckBox(String text);
-    public abstract TextField createField(String text);
-
-}
