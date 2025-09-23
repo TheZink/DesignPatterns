9f9278878b439000206c9c1513328a8903dcc1d3
TheZink <ilkka.sinkonen@gmail.com>
2025-09-23 09:09:55 +0300

    Refactor Main class: move Scanner initialization outside the loop and clean up code structure for better readability

--- builder/src/Main.java
+++ builder/src/Main.java
@@ -3,27 +3,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
         while (true){
-            Scanner scanner = new Scanner(System.in);
+            System.out.println("Welcome to the computer-builder program.\n" +
+                                "Please select, what build do you want\n" +
+                                "1. Gaming computer\n" +
+                                "2. Office computer\n" +
+                                "3. Exit programm");
+            int choice = scanner.nextInt();
 
+            if (choice == 1){
+                ComputerBuilder builder1 = new GamingComputerBuilder();
+                ComputerDirector director1 = new ComputerDirector(builder1);
+                director1.constructGamingComputer();
+                Computer computer1 = builder1.getComputer();
+                System.out.println(computer1);
+            }
 
+            else if (choice == 2){
+                ComputerBuilder builder2 = new OfficeComputerBuilder();
+                ComputerDirector director2 = new ComputerDirector(builder2);
+                director2.constructOfficeComputer();
+                Computer computer2 = builder2.getComputer();
+                System.out.println(computer2);
+            }
+
+            else if (choice == 3){
+                System.out.println("Closing program. Have a nice day!");
+                break;
+            }
+            
+            else {
+                System.out.println("Error! Unknown input");
+            }
         }
-        ComputerBuilder builder1 = new GamingComputerBuilder();
-        ComputerBuilder builder2 = new OfficeComputerBuilder();
-
-        ComputerDirector director1 = new ComputerDirector(builder1);
-        ComputerDirector director2 = new ComputerDirector(builder2);
-
-        director1.constructGamingComputer();
-        director2.constructOfficeComputer();
-
-        Computer computer1 = builder1.getComputer();
-        Computer computer2 = builder2.getComputer();
-
-        System.out.println(computer1);
-        System.out.println(computer2);
-
-
+        scanner.close();
     }
 }
